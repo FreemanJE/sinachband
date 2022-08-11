5b070bbe88ead586b8ace08cb7b78ebd9c7009a0
Freeman <pfreejohnson@gmail.com>
2022-08-11 17:38:51 +0200

    update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,59 +1,42 @@
-import React, { NavLink } from 'react-router-dom'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className="footer" >
-            {/* <nav>
-                footer
-            </nav> */}
+  return (
+    <footer className="footer">
+      Copyright @{" "}
+      <a href="https://github.com/FreemanJE" className="anchorFooter">
+        Etukas
+      </a>
+      <a
+        className="anchor"
+        href="https://www.linkedin.com/in/freeman-johnson-links/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a
+        className="anchor"
+        href="https://github.com/FreemanJE"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fab fa-github-square"></i>
+      </a>
+      <a className="anchor" href=" " target="_blank" rel="noopener noreferrer">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a className="anchor" href=" " target="_blank" rel="noopener noreferrer">
+        <i class="fab fa-youtube"></i>
+      </a>
+      <a className="anchor" href=" " target="_blank" rel="noopener noreferrer">
+        <i class="fab fa-spotify"></i>
+      </a>
+      <a className="anchor" href=" " target="_blank" rel="noopener noreferrer">
+        <i class="fab fa-tiktok"></i>
+      </a>
+    </footer>
+  );
+};
 
-                Copyright: <a href=" " className="anchorFooter">Etukas</a>
-                <a className="anchor"
-                    href="'"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <i class="fab fa-linkedin"></i>
-                </a>
-                <a className="anchor"
-                    href=" "
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <i class="fab fa-github-square"></i>
-                </a>
-                <a className="anchor"
-                    href=" "
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <i class="fab fa-instagram"></i>
-                </a>
-                <a className="anchor"
-                    href=" "
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <i class="fab fa-youtube"></i>
-                </a>
-                <a className="anchor"
-                    href=" "
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <i class="fab fa-spotify"></i>
-                </a>
-                <a className="anchor"
-                    href=" "
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <i class="fab fa-tiktok"></i>
-                </a>
-           
-
-        </footer>
-    )
-}
-
-export default Footer
+export default Footer;
--- src/routes/Vocals.jsx
+++ src/routes/Vocals.jsx
@@ -1,21 +1,23 @@
-import { NavLink } from 'react-router-dom'
-import Pics from '../image/sinach-vocals.jpg'
+import { NavLink } from "react-router-dom";
+import Pics from "../image/sinach-vocals.jpg";
+import Pics2 from "../image/sinach-vocals2.jpeg";
 
 const Vocals = () => {
+  return (
+    <>
+      {/* <h1 style={{color: 'white'}}> Vocals </h1> */}
+      <nav style={{ textAlign: "center" }}>
+        <NavLink className="nav" to="vocals">
+          <img src={Pics} alt="bass" />
+        </NavLink>{" "}
+        |{" "}
+        <NavLink className="nav" to="vocals">
+          <img src={Pics2} alt="bass" />
+        </NavLink>{" "}
+        |{" "}
+      </nav>
+    </>
+  );
+};
 
-    return (
-        <>
-        {/* <h1 style={{color: 'white'}}> Vocals </h1> */}
-            <nav style={{ textAlign: "center" }}>
-            <NavLink className="nav" to='vocals' >
-                    <img src={Pics} alt='bass' />
-                </NavLink> |{" "}
-                <NavLink className="nav" to='vocals' >
-                    <img src={Pics} alt='bass' />
-                </NavLink> |{" "}
-            </nav>
-        </>
-    )
-}
-
-export default Vocals
+export default Vocals;
